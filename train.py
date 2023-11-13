c644e5657a7ca90c68ab843a0e2bc639565015b2
DearFloyd <736762731@qq.com>
2023-11-13 16:19:57 +0000

    update train log

--- train.py
+++ train.py
@@ -3,16 +3,16 @@ warnings.filterwarnings('ignore')
 from ultralytics import RTDETR
 
 if __name__ == '__main__':
-    model = RTDETR('ultralytics/cfg/models/rt-detr/rtdetr-r18.yaml')
-    # model.load('') # loading pretrain weights
-    model.train(data='dataset/data.yaml',
+    model = RTDETR('ultralytics/cfg/models/rt-detr/rtdetr-l.yaml')
+    model.load('/workspace/cv-docker/joey04.li/datasets/master_thesis_rtdetr/weights/rtdetr-l.pt') # loading pretrain weights
+    model.train(data='/workspace/cv-docker/joey04.li/datasets/master_thesis_rtdetr/dataset/classroom_data.yaml',
                 cache=False,
                 imgsz=640,
-                epochs=100,
-                batch=4,
+                epochs=300,
+                batch=48,
                 workers=4,
                 device='0',
                 # resume='', # last.pt path
-                project='runs/train',
-                name='exp',
+                project='/workspace/cv-docker/joey04.li/datasets/master_thesis_rtdetr/outputs/train',
+                name='rtdetr-l-48bs-300ep',
                 )
