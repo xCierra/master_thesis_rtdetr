ec8216362e5510eb388a262bee49a374d826cf28
DearFloyd <736762731@qq.com>
2023-11-14 08:04:16 +0000

    update train log

--- train.py
+++ train.py
@@ -8,11 +8,11 @@ if __name__ == '__main__':
     model.train(data='/workspace/cv-docker/joey04.li/datasets/master_thesis_rtdetr/dataset/classroom_data.yaml',
                 cache=False,
                 imgsz=640,
-                epochs=300,
-                batch=48,
+                epochs=500,
+                batch=64,
                 workers=4,
                 device='0',
                 # resume='', # last.pt path
                 project='/workspace/cv-docker/joey04.li/datasets/master_thesis_rtdetr/outputs/train',
-                name='rtdetr-l-48bs-300ep',
+                name='rtdetr-l-64bs-500ep',
                 )
